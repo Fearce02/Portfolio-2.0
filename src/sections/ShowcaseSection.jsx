78dc93058dee5b4c194b3c94565e961eb306ff54
Pranav <pranav.kumbhar02@gmail.com>
2025-05-27 21:19:41 +0530

    Added Projects Section

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -1,12 +1,93 @@
-import React from "react";
+import React, { useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const ShowcaseSection = () => {
+  const sectionRef = useRef(null);
+  const project1Ref = useRef(null);
+  const project2Ref = useRef(null);
+  const project3Ref = useRef(null);
+
+  useGSAP(() => {
+    const projects = [
+      project1Ref.current,
+      project2Ref.current,
+      project3Ref.current,
+    ];
+    projects.forEach((card, index) => {
+      gsap.fromTo(
+        card,
+        {
+          y: 50,
+          opacity: 0,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 1,
+          delay: 0.3 * (index + 1),
+          scrollTrigger: {
+            trigger: card,
+            start: "top bottom-=10",
+          },
+        }
+      );
+    });
+    gsap.fromTo(
+      sectionRef.current,
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        duration: 1.5,
+      }
+    );
+  }, []);
+
   return (
-    <div id="work" className="app-showcase">
+    <section id="work" ref={sectionRef} className="app-showcase">
       <div className="w-full">
-        <div className="showcaselayout"></div>
+        <div className="showcaselayout">
+          {/* LEFT: Showcase Content */}
+          <div className="first-project-wrapper" ref={project1Ref}>
+            <div className="image-wrapper">
+              <img src="/images/project1.png" alt="Noted" />
+            </div>
+            <div className="text-content">
+              <h2>
+                {" "}
+                Noted: A simple and intuitive task-tracking web app designed for
+                a seamless user experience.{" "}
+              </h2>
+              <p className="text-white-50 md:text-xl">
+                A simple CRUD Todo application built with the MERN stack. It
+                features password hashing, input validation using Zod, and
+                MongoDB for persistent storage.
+              </p>
+            </div>
+          </div>
+          {/* RIGHT: Showcase Content */}
+          <div className="project-list-wrapper overflow-hidden">
+            <div className="project" ref={project2Ref}>
+              <div className="image-wrapper bg-[#ffefdb]">
+                <img src="/images/project2.png" alt="Diet-Tracker" />
+              </div>
+              <h2 className="">Diet Tracker</h2>
+            </div>
+            <div className="project" ref={project3Ref}>
+              <div className="image-wrapper bg-[#ffe7eb]">
+                <img src="/images/project3.png" alt="Discord-BOT" />
+              </div>
+              <h2 className="">Discord Bots</h2>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
