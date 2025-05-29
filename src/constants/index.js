b2c308e4a542de832fd649acb681fe3ddfe4dcd2
Pranav <pranav.kumbhar02@gmail.com>
2025-05-29 20:52:52 +0530

    added Experience section

--- src/constants/index.js
+++ src/constants/index.js
@@ -121,41 +121,29 @@ const techStackIcons = [
 const expCards = [
   {
     review:
-      "Adrian brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
-    imgPath: "/images/exp1.png",
-    logoPath: "/images/logo1.png",
-    title: "Frontend Developer",
-    date: "January 2023 - Present",
+      "Pranav consistently demonstrated a high level of quality and attention to detail in QA testing for AAA sports titles. His contributions improved game stability and the overall player experience.",
+    imgPath: "/images/logos/EA-logo-long.png",
+    logoPath: "/images/logos/EA-logo-short.png",
+    title: "QA tester 2",
+    date: "April 2022 - October 2023",
     responsibilities: [
-      "Developed and maintained user-facing features for the Hostinger website.",
-      "Collaborated closely with UI/UX designers to ensure seamless user experiences.",
-      "Optimized web applications for maximum speed and scalability.",
+      "Conducted functionality and stability testing for EA’s flagship sports games, including FIFA 23 and NHL series.",
+      "Led stability testing efforts as the Point of Contact, mentoring new testers and creating onboarding resources.",
+      "Collaborated with dev and design teams to troubleshoot crashes, analyze system behavior, and optimize game performance.",
+      "Executed comprehensive test suites including regression, performance, and edge case testing.",
     ],
   },
   {
     review:
-      "Adrian’s contributions to Docker's web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
-    imgPath: "/images/exp2.png",
-    logoPath: "/images/logo2.png",
-    title: "Full Stack Developer",
-    date: "June 2020 - December 2023",
+      "Pranav contributed to the successful online performance of F1 2021 through diligent testing and issue resolution. His work helped ensure a seamless multiplayer experience for players.",
+    imgPath: "/images/logos/Codemasters-logo-long.png",
+    logoPath: "/images/logos/Codemasters-logo-short.png",
+    title: "QA tester 1",
+    date: "September 2021 - January-2022",
     responsibilities: [
-      "Led the development of Docker's web applications, focusing on scalability.",
-      "Worked with backend engineers to integrate APIs seamlessly with the frontend.",
-      "Contributed to open-source projects that were used with the Docker ecosystem.",
-    ],
-  },
-  {
-    review:
-      "Adrian’s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.",
-    imgPath: "/images/exp3.png",
-    logoPath: "/images/logo3.png",
-    title: "React Native Developer",
-    date: "March 2019 - May 2020",
-    responsibilities: [
-      "Built cross-platform mobile apps using React Native, integrating with Appwrite's backend services.",
-      "Improved app performance and user experience through code optimization and testing.",
-      "Coordinated with the product team to implement features based on feedback.",
+      "Specialized in online stability testing for F1 2021, preventing crashes, disconnections, and data loss in multiplayer.",
+      "Monitored real-time gameplay sessions to detect performance issues and reported inconsistencies.",
+      "Collaborated with backend and network engineers to address crash reports and enhance server reliability.",
     ],
   },
 ];
